9da67a9835dda568efd0324764286859afa5aaaf
tama <tama@ttk1.net>
2019-11-23 00:07:19 +0900

    hello, react

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React = require('react');
+import ReactDOM = require('react-dom');
+
+window.onload = () => {
+  const element = <h1>Hello, react!</h1>;
+  ReactDOM.render(
+    element,
+    document.body
+  );
+};
