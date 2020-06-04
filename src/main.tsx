92be2be3f47ebcc48ffb5c4e386a0477e4785dcd
tama <tama@ttk1.net>
2020-06-05 04:28:56 +0900

    bootstrapの導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import React = require('react');
-import ReactDOM = require('react-dom');
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+
+const Home = () => (
+  <React.Fragment>
+    <h4 className="text-primary my-2">Hello, World!</h4>
+    <div className="list-group">
+      <Link to="/hoge" className="list-group list-group-item-action">ほげ</Link>
+    </div>
+  </React.Fragment>
+);
+
+const Hoge = () => (
+  <p>Hoge!</p>
+);
 
 window.onload = () => {
-  const element = <h1>Hello, react!</h1>;
   ReactDOM.render(
-    element,
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/hoge" component={Hoge} />
+      </Switch>
+    </Router>,
     document.getElementById('root')
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
         test: /\.tsx?$/,
         exclude: [/node_modules/],
         use: 'ts-loader'
-      }]
+      },
+    {
+      test: /\.css?$/,
+      use: ['style-loader', 'css-loader']
+    }]
   }
 };
